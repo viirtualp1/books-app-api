ca80cef890fe2af5036fe914907d2f54ece78f87
virtualp1 <zininnikita309@gmail.com>
2024-02-25 22:59:59 +0900

    init

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { DatabaseModule } from '../database.module';
+import { bookProviders } from './book.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [BookController],
-  providers: [BookService]
+  providers: [BookService, ...bookProviders]
 })
 export class BookModule {}
